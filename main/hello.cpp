19961ea84f90d986e2785f84a51d1356025260c7
crvarsha0102 <144035005+crvarsha0102@users.noreply.github.com>
2025-03-16 20:00:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World, Hey there\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this. My SRN: PES2UG22CS137 \n";
   return 0;
 }
